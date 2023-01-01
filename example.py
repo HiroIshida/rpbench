11c3bff0fbb85a622184e924831d0abed66ed335
HiroIshida <spitfire.docomo@gmail.com>
2023-01-02 00:36:56 +0900

    example: complete example using rrt and nlp

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 import numpy as np
 from ompl import Algorithm, set_ompl_random_seed
+from skmp.solver.nlp_solver import SQPBasedSolver, SQPBasedSolverConfig
 from skmp.solver.ompl_solver import OMPLSolver, OMPLSolverConfig
 
 from rpbench.world import TabletopBoxRightArmReachingTask, TaskVisualizer
@@ -14,13 +15,15 @@ ompl_solcon = OMPLSolverConfig(n_max_eval=100000, algorithm=Algorithm.RRTConnect
 ompl_sovler = OMPLSolver.setup(problem, ompl_solcon)
 ompl_result = ompl_sovler.solve()
 assert ompl_result.traj is not None
+print(ompl_result.time_elapsed)
 
-# n_wp = 30
-# nlp_solcon = SQPBasedSolverConfig(n_wp, motion_step_satisfaction="explicit")
-# nlp_solver = SQPBasedSolver.setup(problem, nlp_solcon)
-# nlp_result = nlp_solver.solve(ompl_result.traj.resample(n_wp))
-# assert nlp_result.traj is not None
+n_wp = 60
+nlp_solcon = SQPBasedSolverConfig(n_wp, motion_step_satisfaction="post")
+nlp_solver = SQPBasedSolver.setup(problem, nlp_solcon)
+nlp_result = nlp_solver.solve(ompl_result.traj.resample(n_wp))
+assert nlp_result.traj is not None
+print(nlp_result.time_elapsed)
 
 vis = TaskVisualizer(task)
 vis.show()
-vis.visualize_trajectory(ompl_result.traj.resample(20))
+vis.visualize_trajectory(nlp_result.traj.resample(n_wp))
