1da40d2e8bd84fa841630458c05dfda336a6ef58
HiroIshida <spitfire.docomo@gmail.com>
2023-01-05 07:00:41 +0900

    feat: get mesh and vector desc from desc table

--- rpbench/interface.py
+++ rpbench/interface.py
@@ -43,7 +43,7 @@ class WorldBase(ABC):
         ...
 
 
-@dataclass
+@dataclass(frozen=True)
 class DescriptionTable:
     """Unified format of description for all tasks
     both world and descriptions should be encoded into ArrayData
@@ -51,6 +51,59 @@ class DescriptionTable:
 
     world_desc_dict: Dict[str, np.ndarray]  # world common for all sub tasks
     wcond_desc_dicts: List[Dict[str, np.ndarray]]  # world conditioned
+
+    # currently we assume that world and world-conditioned descriptions follows
+    # the following rule.
+    # wd refere to world description and wcd refere to world-condtioned descriotion
+    # - wd must have either 2dim or 3dim array and not both
+    # - wd has only one key for 2dim or 3dim array
+    # - wd may have 1 dim array
+    # - wcd does not have 2dim or 3dim array
+    # - wcd must have 1dim array
+    # - wcd may be empty, but wd must not be empty
+    # to remove the above limitation, create a task class which inherit rpbench Task
+    # which is equipped with desc-2-tensor-tuple conversion rule
+
+    def get_mesh(self) -> np.ndarray:
+        wd_ndim_to_value = {v.ndim: v for v in self.world_desc_dict.values()}
+        ndim_set = wd_ndim_to_value.keys()
+
+        contains_either_2or3_not_both = (2 in ndim_set) ^ (3 in ndim_set)
+        assert contains_either_2or3_not_both
+        if 2 in ndim_set:
+            mesh = wd_ndim_to_value[2]
+        elif 3 in ndim_set:
+            mesh = wd_ndim_to_value[3]
+        else:
+            assert False
+        return mesh
+
+    def get_vector_descs(self) -> List[np.ndarray]:
+        wd_ndim_to_value = {v.ndim: v for v in self.world_desc_dict.values()}
+        ndim_set = wd_ndim_to_value.keys()
+        one_key_per_dim = len(wd_ndim_to_value) == len(self.world_desc_dict)
+        assert one_key_per_dim
+        wd_1dim_desc: Optional[np.ndarray] = None
+        if 1 in ndim_set:
+            wd_1dim_desc = wd_ndim_to_value[1]
+
+        if len(self.wcond_desc_dicts) == 0:
+            # FIXME: when wcd len == 0, wd_1dim_desc_tensor is ignored ...?
+            return []
+        else:
+            wcd_desc_dict = self.wcond_desc_dicts[0]
+            ndims = set([v.ndim for v in wcd_desc_dict.values()])
+            assert ndims == {1}
+
+            np_wcd_desc_list = []
+            for wcd_desc_dict in self.wcond_desc_dicts:
+                wcd_desc_vec_list = []
+                if wd_1dim_desc is not None:
+                    wcd_desc_vec_list.append(wd_1dim_desc)
+                wcd_desc_vec_list.extend(list(wcd_desc_dict.values()))
+                wcd_desc_vec_cat = np.concatenate(wcd_desc_vec_list)
+                np_wcd_desc_list.append(wcd_desc_vec_cat)
+            return np_wcd_desc_list
 
 
 @dataclass
