3d6b20468c7c4791ec65286a21648733f3e5d1b9
HiroIshida <spitfire.docomo@gmail.com>
2023-01-11 01:14:55 +0900

    example: add dual arm example

--- example.py
+++ example.py
@@ -3,29 +3,42 @@ from ompl import Algorithm, set_ompl_random_seed
 from skmp.solver.nlp_solver import SQPBasedSolver, SQPBasedSolverConfig
 from skmp.solver.ompl_solver import OMPLSolver, OMPLSolverConfig
 
-from rpbench.tabletop import TabletopBoxRightArmReachingTask, TaskVisualizer
+from rpbench.tabletop import (
+    TabletopBoxDualArmReachingTask,
+    TabletopBoxRightArmReachingTask,
+    TabletopBoxTaskBase,
+    TaskVisualizer,
+)
 
-set_ompl_random_seed(0)
-np.random.seed(0)
+set_ompl_random_seed(1)
+np.random.seed(2)
 
-task = TabletopBoxRightArmReachingTask.sample(1, True)
+dual = True
+
+task: TabletopBoxTaskBase
+if dual:
+    task = TabletopBoxDualArmReachingTask.sample(1, True)
+else:
+    task = TabletopBoxRightArmReachingTask.sample(1, True)
+
 problem = task.export_problems()[0]
 
-ompl_solcon = OMPLSolverConfig(n_max_call=100000, algorithm=Algorithm.RRTConnect)
+ompl_solcon = OMPLSolverConfig(n_max_call=10000, algorithm=Algorithm.RRTConnect, simplify=True)
 ompl_sovler = OMPLSolver.init(ompl_solcon)
 ompl_sovler.setup(problem)
 ompl_result = ompl_sovler.solve()
 assert ompl_result.traj is not None
 print(ompl_result.time_elapsed)
 
-n_wp = 40
-nlp_solcon = SQPBasedSolverConfig(n_wp, motion_step_satisfaction="explicit")
+n_wp = 30
+nlp_solcon = SQPBasedSolverConfig(n_wp, motion_step_satisfaction="explicit", n_max_call=100)
 nlp_solver = SQPBasedSolver.init(nlp_solcon)
 nlp_solver.setup(problem)
 nlp_result = nlp_solver.solve(ompl_result.traj.resample(n_wp))
+# nlp_result = nlp_solver.solve()
 assert nlp_result.traj is not None
 print(nlp_result.time_elapsed)
 
 vis = TaskVisualizer(task)
 vis.show()
-vis.visualize_trajectory(nlp_result.traj.resample(n_wp))
+vis.visualize_trajectory(nlp_result.traj.resample(30))
