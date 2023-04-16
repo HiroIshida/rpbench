d8da78119c008ef7c63512e632ca04a517934775
HiroIshida <spitfire.docomo@gmail.com>
2023-04-17 03:38:35 +0900

    feat: laddar climb

--- example/humanoid_ladder_climb.py
+++ example/humanoid_ladder_climb.py
@@ -1,6 +1,5 @@
 import time
 
-from skmp.constraint import CollFreeConst, IneqCompositeConst, PoseConstraint
 from skmp.robot.jaxon import Jaxon, JaxonConfig
 from skmp.robot.utils import set_robot_state
 from skmp.satisfy import satisfy_by_optimization_with_budget
@@ -8,36 +7,33 @@ from skrobot.utils.urdf import mesh_simplify_factor
 from skrobot.viewers import TrimeshSceneViewer
 from tinyfk import BaseType
 
-from rpbench.jaxon.ladder import LadderWorld
+from rpbench.jaxon.ladder import ConstraintSequence, LadderWorld
 
 if __name__ == "__main__":
     world = LadderWorld.sample(standard=True)
-    axes = world.third_axes()
-
     with mesh_simplify_factor(0.2):
         jaxon = Jaxon()
-        jaxon_config = JaxonConfig()
-        efkin_legonly = jaxon_config.get_endeffector_kin(rleg=True, lleg=True, rarm=True, larm=True)
-        colkin = jaxon_config.get_collision_kin(rsole=False, lsole=False)
+    jaxon_config = JaxonConfig()
+    cons_seq = ConstraintSequence(world)
 
-        # eq const
-        pose_const = PoseConstraint.from_skrobot_coords(axes, efkin_legonly, jaxon)
-        eq_const = pose_const
-
-        # ineq const
-        col_const = CollFreeConst(colkin, world.get_exact_sdf(), jaxon)
-        selcol_const = jaxon_config.get_neural_selcol_const(jaxon)
-        ineq_const = IneqCompositeConst([col_const, selcol_const])
-
-        # bounds
-        bounds = jaxon_config.get_box_const()
-
-    # solving initial IK
-    print("start solving")
-    result = satisfy_by_optimization_with_budget(
-        eq_const, bounds, ineq_const, None, n_trial_budget=300
-    )
-    assert result.success
+    q_pre = None
+    for mode in [
+        "first",
+        "post_first",
+        "pre_second",
+        "second",
+        "pre_third",
+        "third",
+    ]:
+        eq_const, ineq_const = cons_seq.get_constraint(jaxon, "satisfy", mode)  # type: ignore
+        bounds = jaxon_config.get_close_box_const(q_pre)
+        print("start solving")
+        result = satisfy_by_optimization_with_budget(
+            eq_const, bounds, ineq_const, q_pre, n_trial_budget=300
+        )
+        print(result)
+        q_pre = result.q
+        assert result.success
 
     set_robot_state(
         jaxon, jaxon_config._get_control_joint_names(), result.q, base_type=BaseType.FLOATING
@@ -45,8 +41,6 @@ if __name__ == "__main__":
 
     vis = TrimeshSceneViewer()
     vis.add(jaxon)
-    for ax in axes:
-        vis.add(ax)
     world.visualize(vis)
     vis.show()
     time.sleep(100)
--- rpbench/jaxon/ladder.py
+++ rpbench/jaxon/ladder.py
@@ -1,10 +1,19 @@
 from dataclasses import dataclass
-from typing import List, Type, Union
+from typing import List, Literal, Tuple, Type, Union
 
 import numpy as np
+from skmp.constraint import (
+    AbstractEqConst,
+    AbstractIneqConst,
+    CollFreeConst,
+    IneqCompositeConst,
+    PoseConstraint,
+)
+from skmp.robot.jaxon import Jaxon, JaxonConfig
 from skrobot.coordinates import Coordinates
 from skrobot.model.primitives import Axis, Box
 from skrobot.sdf import UnionSDF
+from skrobot.utils.urdf import mesh_simplify_factor
 from skrobot.viewers import TrimeshSceneViewer
 from voxbloxpy.core import Grid
 
@@ -98,23 +107,80 @@ class LadderWorld:
         for step in self.steps:
             viewer.add(step)
 
-    def first_axes(self) -> List[Axis]:
-        step = self.steps[0]
+
+@dataclass
+class ConstraintSequence:
+    world: LadderWorld
+
+    def __getitem__(self, index):
+        modes = ["first", "post_first", "pre_second", "second", "post_second", "pre_third", "third"]
+        return self.get_constraint(modes[index])
+
+    def get_constraint(
+        self,
+        jaxon: Jaxon,
+        mode: Literal["satisfy", "motion_planning"],
+        phase: Literal[
+            "first", "post_first", "pre_second", "second", "post_second", "pre_third", "third"
+        ],
+    ) -> Tuple[AbstractEqConst, AbstractIneqConst]:
+
+        # dirty hack to call e.g. self._first_axes
+        method_name = "_" + phase + "_axes"
+        axes: List[Coordinates] = getattr(self, method_name)()
+
+        with mesh_simplify_factor(0.2):
+            jaxon_config = JaxonConfig()
+
+            # determine eq const
+            if mode == "satisfy":
+                efkin = jaxon_config.get_endeffector_kin()
+            else:
+                if phase == "pre_second":
+                    efkin = jaxon_config.get_endeffector_kin(
+                        rleg=False, lleg=True, rarm=True, larm=True
+                    )
+                elif phase == "pre_third":
+                    efkin = jaxon_config.get_endeffector_kin(
+                        rleg=True, lleg=False, rarm=True, larm=True
+                    )
+                else:
+                    assert False
+            pose_const = PoseConstraint.from_skrobot_coords(axes, efkin, jaxon)
+            eq_const = pose_const
+
+            # determine ineq const
+            if phase in ["first", "second", "third"]:
+                colkin = jaxon_config.get_collision_kin(rsole=False, lsole=False)
+            elif phase in ["post_first", "pre_second"]:
+                colkin = jaxon_config.get_collision_kin(rsole=True, lsole=False)
+            elif phase in ["post_second", "pre_third"]:
+                colkin = jaxon_config.get_collision_kin(rsole=False, lsole=True)
+            else:
+                assert False
+            col_const = CollFreeConst(colkin, self.world.get_exact_sdf(), jaxon)
+            selcol_const = jaxon_config.get_neural_selcol_const(jaxon)
+            ineq_const = IneqCompositeConst([selcol_const, col_const])
+
+            return eq_const, ineq_const
+
+    def _first_axes(self) -> List[Axis]:
+        step = self.world.steps[0]
         depth, _, height = step._extents
 
         rleg_pose: Coordinates = step.copy_worldcoords()
-        rleg_pose.translate([-depth * 0.5 + 0.01, -0.12, height * 0.5])
+        rleg_pose.translate([-depth * 0.5 - 0.08, -0.12, height * 0.5])
         ax1 = Axis.from_coords(rleg_pose)
 
         lleg_pose: Coordinates = step.copy_worldcoords()
-        lleg_pose.translate([-depth * 0.5 + 0.01, 0.12, height * 0.5])
+        lleg_pose.translate([-depth * 0.5 - 0.08, 0.12, height * 0.5])
         ax2 = Axis.from_coords(lleg_pose)
 
-        rarm_pose: Coordinates = self.poles[0].copy_worldcoords()
-        rarm_pose.translate([0.0, 0.0, 0.3], wrt="local")
+        rarm_pose: Coordinates = self.world.poles[0].copy_worldcoords()
+        rarm_pose.translate([0.0, 0.0, 0.5], wrt="local")
 
         larm_pose: Coordinates = rarm_pose.copy_worldcoords()
-        larm_pose.translate([0.0, +self.param.ladder_width, 0.0], wrt="local")
+        larm_pose.translate([0.0, +self.world.param.ladder_width, 0.0], wrt="local")
 
         rarm_pose.rotate(np.pi * 0.5, "x", wrt="local")
         rarm_pose.rotate(-np.pi * 0.3, "y", wrt="local")
@@ -125,46 +191,46 @@ class LadderWorld:
         ax4 = Axis.from_coords(larm_pose)
         return [ax1, ax2, ax3, ax4]
 
-    def post_first_axes(self) -> List[Axis]:
-        ax_rleg, ax_lleg, ax_rarm, ax_larm = self.first_axes()
+    def _post_first_axes(self) -> List[Axis]:
+        ax_rleg, ax_lleg, ax_rarm, ax_larm = self._first_axes()
         pose_rleg = ax_rleg.copy_worldcoords()
-        pose_rleg.translate([0, 0, 0.05])
+        pose_rleg.translate([0, 0, 0.03])
         return [Axis.from_coords(pose_rleg), ax_lleg, ax_rarm, ax_larm]
 
-    def second_axes(self) -> List[Axis]:
-        desired_trans = self.desired_translation_by_step()
-        ax_rleg, ax_lleg, ax_rarm, ax_larm = self.first_axes()
+    def _second_axes(self) -> List[Axis]:
+        desired_trans = self._desired_translation_by_step()
+        ax_rleg, ax_lleg, ax_rarm, ax_larm = self._first_axes()
         pose_rleg = ax_rleg.copy_worldcoords()
         pose_rleg.translate(desired_trans)
         return [Axis.from_coords(pose_rleg), ax_lleg, ax_rarm, ax_larm]
 
-    def pre_second_axes(self) -> List[Axis]:
-        ax_rleg, ax_lleg, ax_rarm, ax_larm = self.second_axes()
+    def _pre_second_axes(self) -> List[Axis]:
+        ax_rleg, ax_lleg, ax_rarm, ax_larm = self._second_axes()
         pose_rleg = ax_rleg.copy_worldcoords()
-        pose_rleg.translate([0, 0, 0.05])
+        pose_rleg.translate([0, 0, 0.03])
         return [Axis.from_coords(pose_rleg), ax_lleg, ax_rarm, ax_larm]
 
-    def post_second_axes(self) -> List[Axis]:
-        ax_rleg, ax_lleg, ax_rarm, ax_larm = self.second_axes()
+    def _post_second_axes(self) -> List[Axis]:
+        ax_rleg, ax_lleg, ax_rarm, ax_larm = self._second_axes()
         pose_lleg = ax_lleg.copy_worldcoords()
-        pose_lleg.translate([0, 0, 0.05])
+        pose_lleg.translate([0, 0, 0.03])
         return [ax_rleg, Axis.from_coords(pose_lleg), ax_rarm, ax_larm]
 
-    def third_axes(self) -> List[Axis]:
-        ax_rleg, ax_lleg, ax_rarm, ax_larm = self.second_axes()
+    def _third_axes(self) -> List[Axis]:
+        ax_rleg, ax_lleg, ax_rarm, ax_larm = self._second_axes()
         pose_lleg = ax_lleg.copy_worldcoords()
-        desired_trans = self.desired_translation_by_step()
+        desired_trans = self._desired_translation_by_step()
         pose_lleg.translate(desired_trans)
         return [ax_rleg, Axis.from_coords(pose_lleg), ax_rarm, ax_larm]
 
-    def pre_third_axes(self) -> List[Axis]:
-        ax_rleg, ax_lleg, ax_rarm, ax_larm = self.third_axes()
+    def _pre_third_axes(self) -> List[Axis]:
+        ax_rleg, ax_lleg, ax_rarm, ax_larm = self._third_axes()
         pose_lleg = ax_lleg.copy_worldcoords()
-        pose_lleg.translate([0, 0, 0.05])
+        pose_lleg.translate([0, 0, 0.03])
         return [ax_rleg, Axis.from_coords(pose_lleg), ax_rarm, ax_larm]
 
-    def desired_translation_by_step(self) -> np.ndarray:
-        step0 = self.steps[0]
-        step1 = self.steps[1]
+    def _desired_translation_by_step(self) -> np.ndarray:
+        step0 = self.world.steps[0]
+        step1 = self.world.steps[1]
         diff = step1.worldpos() - step0.worldpos()
         return diff
