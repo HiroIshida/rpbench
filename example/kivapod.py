89a89de5412e5d1ca36ce88bc1f15c1478c59a48
HiroIshida <spitfire.docomo@gmail.com>
2023-04-27 18:01:23 +0900

    refactor: use viewer of skmp

--- example/kivapod.py
+++ example/kivapod.py
@@ -2,21 +2,22 @@ import time
 
 import numpy as np
 
-from rpbench.pr2.common import InteractiveTaskVisualizer
+from rpbench.pr2.common import PR2InteractiveTaskVisualizer, PR2StaticTaskVisualizer
 from rpbench.pr2.kivapod import KivapodEmptyReachingTask
 
 np.random.seed(3)
 
-# task = KivapodEmptyReachingTask.sample(30, standard=False)
-# vis = InteractiveTaskVisualizer(task)
-# vis.show()
-# time.sleep(100)
+save_visualization_result = False
 
 task = KivapodEmptyReachingTask.sample(1, standard=True)
 res = task.solve_default()[0]
 assert res.traj is not None
 
-vis = InteractiveTaskVisualizer(task)
-vis.show()
-vis.visualize_trajectory(res.traj.resample(30))
-time.sleep(2000)
+if save_visualization_result:
+    static_vis = PR2StaticTaskVisualizer.from_task(task)
+    static_vis.save_trajectory_gif(res.traj.resample(30), "./kivapod_result.gif")
+else:
+    interactive_vis = PR2InteractiveTaskVisualizer.from_task(task)
+    interactive_vis.show()
+    interactive_vis.visualize_trajectory(res.traj.resample(30))
+    time.sleep(2000)
--- rpbench/pr2/common.py
+++ rpbench/pr2/common.py
@@ -1,19 +1,6 @@
-import copy
-import time
 from abc import ABC, abstractmethod
 from functools import lru_cache
-from pathlib import Path
-from typing import (
-    Callable,
-    ClassVar,
-    Generic,
-    List,
-    Protocol,
-    Tuple,
-    Type,
-    TypeVar,
-    Union,
-)
+from typing import Callable, List, Type, TypeVar
 
 import numpy as np
 from skmp.constraint import (
@@ -29,15 +16,17 @@ from skmp.kinematics import (
 )
 from skmp.robot.pr2 import PR2Config
 from skmp.robot.utils import get_robot_state, set_robot_state
-from skmp.trajectory import Trajectory
+from skmp.visualization.solution_visualizer import (
+    InteractiveSolutionVisualizer,
+    SolutionVisualizerBase,
+    StaticSolutionVisualizer,
+)
 from skrobot.coordinates import Coordinates
-from skrobot.model import RobotModel
 from skrobot.model.primitives import Axis
 from skrobot.models.pr2 import PR2
-from skrobot.viewers import TrimeshSceneViewer
 from tinyfk import BaseType
 
-from rpbench.utils import SceneWrapper
+from rpbench.interface import TaskBase
 
 
 class CachedPR2ConstProvider(ABC):
@@ -150,142 +139,41 @@ class CachedDualArmPR2ConstProvider(CachedPR2ConstProvider):
         return IneqCompositeConst([colfree, selcolfree])
 
 
-ViewerT = TypeVar("ViewerT", bound=Union[TrimeshSceneViewer, SceneWrapper])
+t = np.array(
+    [
+        [-0.74452768, 0.59385861, -0.30497620, -0.28438419],
+        [-0.66678662, -0.68392597, 0.29604201, 0.80949977],
+        [-0.03277405, 0.42376552, 0.90517879, 3.65387983],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
+)
 
 
-class VisualizableWorld(Protocol):
-    def visualize(self, viewer: Union[TrimeshSceneViewer, SceneWrapper]) -> None:
-        ...
+PR2SolutionViewerT = TypeVar("PR2SolutionViewerT", bound="PR2SolutionViewerBase")
 
 
-class VisualizableTask(Protocol):
-    config_provider: ClassVar[Type[CachedPR2ConstProvider]]
-    descriptions: List[Tuple[Coordinates, ...]]
-
-    @property
-    def world(self) -> VisualizableWorld:
-        ...
-
-    #  https://github.com/python/mypy/issues/7041
-
-
-class TaskVisualizerBase(Generic[ViewerT], ABC):
-    # TODO: this class actually take any Task if it has config provider
-    task: VisualizableTask
-    viewer: ViewerT
-    robot_model: RobotModel
-    _show_called: bool
-
-    def __init__(self, task: VisualizableTask):
-        viewer = self.viewer_type()()
-
-        robot_config = task.config_provider()
-        robot_model = robot_config.get_pr2()
-        viewer.add(robot_model)
-
-        task.world.visualize(viewer)
-        for desc in task.descriptions:
-            for co in desc:
-                axis = Axis.from_coords(co)
-                viewer.add(axis)
-
-        self.task = task
-        self.viewer = viewer
-        self.robot_model = robot_model
-        self._show_called = False
-
-    def update_robot_state(self, q: np.ndarray) -> None:
-        robot_config_provider = self.task.config_provider()
-        config = robot_config_provider.get_config()
-        set_robot_state(self.robot_model, config._get_control_joint_names(), q, config.base_type)
-
+class PR2SolutionViewerBase(SolutionVisualizerBase):
     @classmethod
-    @abstractmethod
-    def viewer_type(cls) -> Type[ViewerT]:
-        ...
+    def from_task(cls: Type[PR2SolutionViewerT], task: TaskBase) -> PR2SolutionViewerT:
+        assert len(task.descriptions) == 1
+        geometries = []
+        for co in task.descriptions[0]:
+            geometries.append(Axis.from_coords(co))
+
+        config: PR2Config = task.config_provider.get_config()
+        pr2 = task.config_provider.get_pr2()
+
+        def robot_updator(robot, q):
+            set_robot_state(pr2, config._get_control_joint_names(), q, config.base_type)
+
+        obj = cls(pr2, geometry=geometries, visualizable=task.world, robot_updator=robot_updator)
+        obj.viewer.camera_transform = t
+        return obj
 
 
-class InteractiveTaskVisualizer(TaskVisualizerBase[TrimeshSceneViewer]):
-    def show(self) -> None:
-        self.viewer.show()
-        time.sleep(1.0)
-        self._show_called = True
-
-    def visualize_trajectory(self, trajectory: Trajectory, t_interval: float = 0.6) -> None:
-        assert self._show_called
-        robot_config_provider = self.task.config_provider()
-
-        q_end = trajectory.numpy()[-1]
-        self.update_robot_state(q_end)
-
-        robot_model = robot_config_provider.get_pr2()
-        config = robot_config_provider.get_config()
-
-        for q in trajectory.numpy()[:-1]:
-            set_robot_state(robot_model, config._get_control_joint_names(), q, config.base_type)
-            self.viewer.redraw()
-            time.sleep(t_interval)
-
-        print("==> Press [q] to close window")
-        while not self.viewer.has_exit:
-            time.sleep(0.1)
-            self.viewer.redraw()
-
-    @classmethod
-    def viewer_type(cls) -> Type[TrimeshSceneViewer]:
-        return TrimeshSceneViewer
+class PR2StaticTaskVisualizer(StaticSolutionVisualizer, PR2SolutionViewerBase):
+    ...
 
 
-class StaticTaskVisualizer(TaskVisualizerBase[SceneWrapper]):
-    @classmethod
-    def viewer_type(cls) -> Type[SceneWrapper]:
-        return SceneWrapper
-
-    def save_image(self, path: Union[Path, str]) -> None:
-        if isinstance(path, str):
-            path = Path(path)
-        png = self.viewer.save_image(resolution=[640, 480], visible=True)
-        with path.open(mode="wb") as f:
-            f.write(png)
-
-    def save_trajectory_image(self, trajectory: Trajectory, path: Union[Path, str]) -> None:
-        # self.set_robot_alpha(self.robot_model, 30)
-
-        robot_config_provider = self.task.config_provider()
-        robot_model = robot_config_provider.get_pr2()
-
-        config = robot_config_provider.get_config()
-
-        for q in trajectory.numpy():
-            robot_model_copied = copy.deepcopy(robot_model)
-            set_robot_state(
-                robot_model_copied, config._get_control_joint_names(), q, config.base_type
-            )
-            self.set_robot_alpha(robot_model_copied, 30)
-            self.viewer.add(robot_model_copied)
-
-        robot_model_copied = copy.deepcopy(robot_model)
-        set_robot_state(
-            robot_model_copied,
-            config._get_control_joint_names(),
-            trajectory.numpy()[-1],
-            config.base_type,
-        )
-        self.viewer.add(robot_model_copied)
-
-        if isinstance(path, str):
-            path = Path(path)
-        png = self.viewer.save_image(resolution=[640, 480], visible=True)
-        with path.open(mode="wb") as f:
-            f.write(png)
-
-    @staticmethod
-    def set_robot_alpha(robot: RobotModel, alpha: int):
-        assert alpha < 256
-        for link in robot.link_list:
-            visual_mesh = link.visual_mesh
-            if isinstance(visual_mesh, list):
-                for mesh in visual_mesh:
-                    mesh.visual.face_colors[:, 3] = alpha
-            else:
-                visual_mesh.visual.face_colors[:, 3] = alpha
+class PR2InteractiveTaskVisualizer(InteractiveSolutionVisualizer, PR2SolutionViewerBase):
+    ...
