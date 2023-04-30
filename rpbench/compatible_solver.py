43ef32b629b2de0acb23f3695f0ee4bb8272c3a6
HiroIshida <spitfire.docomo@gmail.com>
2023-04-30 17:56:01 +0900

    fix: add more compatible solvers

--- rpbench/compatible_solver.py
+++ rpbench/compatible_solver.py
@@ -59,13 +59,34 @@ class CompatibleSolvers:
         trajlib_dataset = PlanningDataset.load(task_type)
 
         myrrt_config = MyRRTConfig(3000, satisfaction_conf=SatisfactionConfig(n_max_eval=30))
-        compat_solvers["rrtconnect"] = SkmpTaskSolver.init(
-            MyRRTConnectSolver.init(myrrt_config), task_type
-        )
+        myrrt = MyRRTConnectSolver.init(myrrt_config)
+        myrrt_parallel4 = myrrt.as_parallel_solver(n_process=4)
+        myrrt_parallel8 = myrrt.as_parallel_solver(n_process=8)
+
+        compat_solvers["rrtconnect"] = SkmpTaskSolver.init(myrrt, task_type)
+        compat_solvers["rrtconnect4"] = SkmpTaskSolver.init(myrrt_parallel4, task_type)
+        compat_solvers["rrtconnect8"] = SkmpTaskSolver.init(myrrt_parallel8, task_type)
 
         sqp_config = SQPBasedSolverConfig(30, motion_step_satisfaction="explicit")
-        compat_solvers["memmo_nn"] = DatadrivenTaskSolver.init(
-            NnMemmoSolver, sqp_config, trajlib_dataset
+        print("memmo nn 100")
+        compat_solvers["memmo_nn100"] = DatadrivenTaskSolver.init(
+            NnMemmoSolver,
+            sqp_config,
+            trajlib_dataset,
+            n_data_use=100,
         )
-
+        print("memmo nn 1000")
+        compat_solvers["memmo_nn1000"] = DatadrivenTaskSolver.init(
+            NnMemmoSolver,
+            sqp_config,
+            trajlib_dataset,
+            n_data_use=1000,
+        )
+        print("memmo nn 10000")
+        compat_solvers["memmo_nn10000"] = DatadrivenTaskSolver.init(
+            NnMemmoSolver,
+            sqp_config,
+            trajlib_dataset,
+            n_data_use=10000,
+        )
         return compat_solvers
