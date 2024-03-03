fca169db8dda0e60bb16252a904bcf061193ad25
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-03-03 11:16:51 +0900

    0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = ["numpy", "scikit-motionplan", "voxbloxpy", "threadpoolctl", 
 
 setup(
     name="rpbench",
-    version="0.0.1",
+    version="0.0.2",
     description="Robot planning benchmark",
     author="Hirokazu Ishida",
     author_email="h-ishida@jsk.imi.i.u-tokyo.ac.jp",
