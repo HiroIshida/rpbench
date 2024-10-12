9f0fd735e8144db9b1c8007b18854e1cb9e8a0ff
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-10-13 03:46:21 +0900

    example(jail): plainmp based planning example

--- rpbench/articulated/world/jail.py
+++ rpbench/articulated/world/jail.py
@@ -3,8 +3,9 @@ from dataclasses import dataclass
 from typing import Callable, ClassVar, List, Union
 
 import numpy as np
+from plainmp.psdf import UnionSDF as pUnionSDF
 from scipy.spatial import KDTree
-from skrobot.model.primitives import PointCloudLink
+from skrobot.model.primitives import Axis, PointCloudLink
 from skrobot.sdf import UnionSDF
 from skrobot.viewers import PyrenderViewer, TrimeshSceneViewer
 
@@ -48,7 +49,7 @@ class JailWorld(SamplableWorldBase):
         for panel in panels:
             region.assoc(panel)
         # slide jail
-        region.translate([0.65, 0, 0.7])
+        region.translate([0.7, 0, 0.6])
         return region, panels
 
     @classmethod
@@ -92,12 +93,24 @@ class JailWorld(SamplableWorldBase):
         plink = PointCloudLink(cloud)
         viewer.add(plink)
 
-    def get_sdf(self) -> Callable[[np.ndarray], np.ndarray]:
+    def get_plainmp_sdf(self):
+        cloud_sdf = CloudSDF(self.voxels.to_points(), 0.0)
+        sdf_list = [cloud_sdf]
+        for panel in self.panels:
+            sdf = sksdf_to_cppsdf(panel.sdf)
+            sdf_list.append(sdf)
+        union_sdf = pUnionSDF(sdf_list, False)
+        return union_sdf
+
+    def get_sdf(self, optional_sdfs=None) -> Callable[[np.ndarray], np.ndarray]:
         kdtree = KDTree(self.voxels.to_points())
+
+        if optional_sdfs is None:
+            optional_sdfs = []
 
         def sdf(X: np.ndarray) -> np.ndarray:
             dists, _ = kdtree.query(X)
-            for panel in self.panels:
+            for panel in self.panels + optional_sdfs:
                 dists = np.minimum(dists, panel.sdf(X))
             return dists
 
@@ -114,25 +127,68 @@ class JailWorld(SamplableWorldBase):
 
 
 if __name__ == "__main__":
-    import tqdm
+    from plainmp.ik import solve_ik
+    from plainmp.ompl_solver import OMPLSolver, OMPLSolverConfig
+    from plainmp.problem import Problem
+    from plainmp.psdf import CloudSDF
+    from plainmp.robot_spec import FetchSpec
+    from plainmp.utils import set_robot_state, sksdf_to_cppsdf
     from skrobot.models.fetch import Fetch
 
-    for _ in tqdm.tqdm(range(20)):
-        world = JailWorld.sample()
-        # n_bytes = len(world.serialize())
-        # print(f"n_bytes: {n_bytes}")
+    np.random.seed(3)
+    world = JailWorld.sample()
+    sdf = world.get_plainmp_sdf()
+    co = world.region.copy_worldcoords()
+    co.translate([0.25, 0.05, 0.0])
+    axis = Axis.from_coords(co)
 
-    world_rt = JailWorld.deserialize(world.serialize())
+    fs = FetchSpec()
+    eq_cst = fs.create_gripper_pose_const(co.worldpos())
+    ineq_cst = fs.create_collision_const()
+    ineq_cst.set_sdf(sdf)
+    lb, ub = fs.angle_bounds()
+
+    start = np.array([0.0, 1.31999949, 1.40000015, -0.20000077, 1.71999929, 0.0, 1.6600001, 0.0])
+    msbox = np.array([0.02, 0.02, 0.02, 0.05, 0.05, 0.05, 0.1, 0.1])
+    solver = OMPLSolver(OMPLSolverConfig(50000000, algorithm_range=None, simplify=True))
+
     ts = time.time()
-    sdf = world_rt.get_sdf()
+    while True:
+        ret_ik = solve_ik(eq_cst, ineq_cst, lb, ub, q_seed=None, max_trial=100)
+        if not ineq_cst.is_valid(ret_ik.q):
+            continue
+        goal = ret_ik.q
+        problem = Problem(start, lb, ub, goal, ineq_cst, None, msbox)
+        ret = solver.solve(problem)
+        if ret.traj is not None:
+            break
     print(f"elapsed time: {time.time() - ts}")
-    viewer = PyrenderViewer()
-    # viewer = TrimeshSceneViewer()
-    world_rt.visualize(viewer)
+
+    co = world.region.copy_worldcoords()
+    co.translate([0.27, 0.04, 0.0])
+    eq_cst = fs.create_gripper_pose_const(co.worldpos())
+    problem = Problem(start, lb, ub, eq_cst, ineq_cst, None, msbox)
+
+    solver = OMPLSolver(
+        OMPLSolverConfig(
+            50000000, algorithm_range=None, simplify=True, ertconnect_eps=0.1, n_max_ik_trial=1
+        )
+    )
+    ret = solver.solve(problem, ret.traj)
+    print(ret)
+
     fetch = Fetch()
-    fetch.reset_pose()
+    set_robot_state(fetch, fs.control_joint_names, ret_ik.q)
+    viewer = PyrenderViewer()
+    world.visualize(viewer)
     viewer.add(fetch)
+    viewer.add(axis)
     viewer.show()
+
+    for q in ret.traj:
+        set_robot_state(fetch, fs.control_joint_names, q)
+        time.sleep(1)
+        viewer.redraw()
     import time
 
     time.sleep(1000)
