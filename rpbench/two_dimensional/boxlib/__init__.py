df23f3a484e048cd30ace4588945405878f28d82
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-11-01 16:15:46 +0900

    Revert xmin and xmax of the obstacle in parametric obstacle position

--- rpbench/two_dimensional/boxlib/__init__.py
+++ rpbench/two_dimensional/boxlib/__init__.py
@@ -182,8 +182,8 @@ class ParametricCircles:
 
     @classmethod
     def sample(cls, n: int):
-        x_min = 0.0
-        x_max = 1.0
+        x_min = cls.obstacle_w / 2
+        x_max = 1.0 - cls.obstacle_w / 2
         params = np.random.uniform(x_min, x_max, n)
         return cls(params)
 
